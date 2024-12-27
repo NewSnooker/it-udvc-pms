e7fc41d61c7ec6e2c8973ea7a1f61970e0afd118
NewSnooker <nu0075533@gmail.com>
2024-12-28 02:44:38 +0700

    test generateMetadata PortfolioPage

--- app/(portfolio)/portfolio/[slug]/page.tsx
+++ app/(portfolio)/portfolio/[slug]/page.tsx
@@ -11,51 +11,22 @@ import React from "react";
 import { Metadata } from "next";
 import { WEBSITE_NAME } from "@/constants";
 
+// metadata.tsx
 export async function generateMetadata({
   params,
-  searchParams,
 }: {
   params: { slug: string };
-  searchParams: { [key: string]: string | string[] | undefined };
 }): Promise<Metadata> {
   const portfolioName = decodeURIComponent(params.slug);
   const baseUrl = "https://it-udvc-pms.vercel.app";
-  const { id = "" } = searchParams;
-  if (!id) {
-    return notFound();
-  }
-  const profile = await getPortfolioByUserId(id as string);
-  const profileImage =
-    profile?.profileImage ||
-    "https://utfs.io/f/59b606d1-9148-4f50-ae1c-e9d02322e834-2558r.png";
 
   return {
     title: `Portfolio ${portfolioName} | ${WEBSITE_NAME}`,
-    description: `Portfolio ${portfolioName} ใน ${WEBSITE_NAME} ระบบบริหารจัดการโครงการที่ครบครันและปรับแต่งได้`,
-    openGraph: {
-      title: `Portfolio ${portfolioName} | ${WEBSITE_NAME}`,
-      description: `Portfolio ${portfolioName} ใน ${WEBSITE_NAME} ระบบบริหารจัดการโครงการที่ครบครันและปรับแต่งได้`,
-      url: `${baseUrl}/portfolio/${portfolioName}`,
-      type: "profile",
-      images: [
-        {
-          url: `${baseUrl}${profileImage}`,
-          width: 1200,
-          height: 630,
-          alt: `ภาพโปรไฟล์ของ ${portfolioName}`,
-        },
-      ],
-    },
-    twitter: {
-      card: "summary_large_image",
-      title: `Portfolio ${portfolioName} | ${WEBSITE_NAME}`,
-      description: `Portfolio ${portfolioName} ใน ${WEBSITE_NAME} ระบบบริหารจัดการโครงการที่ครบครันและปรับแต่งได้`,
-      images: [`${baseUrl}${profileImage}`],
-    },
+    description: `Portfolio ${portfolioName} ใน ${WEBSITE_NAME} เป็นข้อมูลเกี่ยวกับโครงการและผู้มีส่วนเกี่ยวข้องในแพลตฟอร์มของเรา`,
   };
 }
 
-export default async function page({
+export default async function Page({
   params,
   searchParams,
 }: {
@@ -66,18 +37,30 @@ export default async function page({
   if (!id) {
     return notFound();
   }
+
   const user = await getAuthUser();
   const profile = await getPortfolioByUserId(id as string);
   const projects = (await getUserPublicProjects(id as string)) || [];
   const count = (await getUserProjectsCount(user?.id)) ?? 0;
-
+  const portfolioName = decodeURIComponent(params.slug);
+  const profileImage =
+    profile?.profileImage ||
+    "https://utfs.io/f/59b606d1-9148-4f50-ae1c-e9d02322e834-2558r.png";
   return (
-    <div className="">
-      <PortfolioPage
-        projects={projects}
-        profile={profile as PortfolioProfile}
-        count={count}
-      />
-    </div>
+    <>
+      <head>
+        <meta property="og:title" content={`Portfolio ${portfolioName}`} />
+        <meta property="og:image" content={profileImage} />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:image" content={profileImage} />
+      </head>
+      <div>
+        <PortfolioPage
+          projects={projects}
+          profile={profile as PortfolioProfile}
+          count={count}
+        />
+      </div>
+    </>
   );
 }
