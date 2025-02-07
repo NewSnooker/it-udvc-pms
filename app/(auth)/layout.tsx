8335e945aefee226d4ec37bff4659084a39dc81b
NewSnooker <nu0075533@gmail.com>
2025-02-07 15:31:02 +0700

    feat: pass email to Footer component in HomeLayout for improved functionality

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -2,7 +2,6 @@ import Footer from "@/components/frontend/site-footer";
 import SiteHeader from "@/components/frontend/site-header";
 import { WEBSITE_NAME } from "@/constants";
 import { Metadata } from "next";
-import { getServerSession } from "next-auth";
 import React, { ReactNode } from "react";
 export const metadata: Metadata = {
   title: {
@@ -10,14 +9,17 @@ export const metadata: Metadata = {
     default: `${WEBSITE_NAME}`,
   },
 };
+
 export default function HomeLayout({ children }: { children: ReactNode }) {
   const session = null;
+  const email = process.env.NODEMAILER_USER;
+
   return (
     <div className="bg-white">
       <SiteHeader session={session} />
       <div className="relative isolate px-6 pt-14 lg:px-8">
         {children}
-        <Footer />
+        <Footer email={email as string} />
       </div>
     </div>
   );
